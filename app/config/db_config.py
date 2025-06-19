6a1d360c86d48341e1fb8ce13ad14787fd68ba14
ipiloni <ignaciopiloni@hotmail.com>
2025-06-19 11:13:01 -0300

    Inserta en BDD correctamente

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,8 +1,17 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///../phishintel.db"
+# Ruta absoluta al archivo de base de datos
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, "../../phishintel.db")
 
-engine = create_engine(DATABASE_URL, echo=True)
+# Conexión a la base
+engine = create_engine(f"sqlite:///{db_path}")
+
+# Session
 SessionLocal = sessionmaker(bind=engine)
+
+# Base declarativa
 Base = declarative_base()
