306093056be32e47bc5a6938a6af688bde923c3c
ipiloni <ignaciopiloni@hotmail.com>
2025-06-19 11:03:40 -0300

    funciona el endpoint

--- app/controllers/usuariosController.py
+++ app/controllers/usuariosController.py
@@ -1,22 +1,26 @@
-from flask import jsonify, request
-from app.main import app
+from flask import jsonify
 from app.backend.models import Usuario
 from app.backend.models.error import responseError
 from app.config.db_config import SessionLocal
+from app.utils.hash import hash_password
 
-@app.route("/usuarios", methods=["POST"])
-def crearUsuario():
-    data = request.get_json()
+class UsuariosController:
 
-    if not data or "idUsuario" not in data or "password" not in data or "nombre" not in data or "apellido" not in data:
-        return responseError("CAMPOS_OBLIGATORIOS", "Faltan campos obligatorios para crear el usuario", 400)
+    def crearUsuario(data):
+        if not data or "idUsuario" not in data or "password" not in data or "nombre" not in data or "apellido" not in data:
+            return responseError("CAMPOS_OBLIGATORIOS", "Faltan campos obligatorios para crear el usuario", 400)
 
-    nuevoUsuario = Usuario(idUsuario = data["idUsuario"], password = data["password"], nombre = data["nombre"], apellido = data["apellido"])
+        hashedPassword = hash_password(data["password"])
 
-    session = SessionLocal()
-    session.add(nuevoUsuario)
-    session.commit()
-    session.refresh(nuevoUsuario)
-    session.close()
+        nuevoUsuario = Usuario(idUsuario=data["idUsuario"],
+                               password=hashedPassword,
+                               nombre=data["nombre"],
+                               apellido=data["apellido"])
 
-    return jsonify({"mensaje": "Usuario creado correctamente"}), 201
+        session = SessionLocal()
+        session.add(nuevoUsuario)
+        session.commit()
+        session.refresh(nuevoUsuario)
+        session.close()
+
+        return jsonify({"mensaje": "Usuario creado correctamente"}), 201
--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, request
 from sqlalchemy import create_engine
-from sqlalchemy.dialects.postgresql import aggregate_order_by
 from sqlalchemy.orm import sessionmaker
 
+from app.controllers.usuariosController import UsuariosController
+
+# Flask es la libreria que vamos a usar para generar los Endpoints
 app = Flask(__name__)
 
-engine = create_engine("sqlite:///phishintel.db")
+# Generamos la base de datos
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, "phishintel.db")
+engine = create_engine(f"sqlite:///{db_path}")
 SessionLocal = sessionmaker(bind=engine)
 
-from app.controllers import usuariosController
+########## ENDPOINTS ##########
+@app.route("/usuarios", methods=["POST"])
+def crearUsuario():
+    data = request.get_json()
+    return UsuariosController.crearUsuario(data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
