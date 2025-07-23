9ffd50f129c5679b46273651bdb36d95c79829c0
ipiloni <ignaciopiloni@hotmail.com>
2025-07-23 18:07:53 -0300

    agrego llamadas desde Twilio (no funcionan pero esta armado el endpoint)

---
+++ app/controllers/llamadasController.py
@@ -0,0 +1,17 @@
+from app.backend.apis.twilio import twilio
+from app.backend.models.error import responseError
+
+class LlamadasController:
+
+    @staticmethod
+    def llamar(data):
+        if not data or "destinatario" not in data or "remitente" not in data:
+            return responseError("CAMPOS_OBLIGATORIOS", "Falta el campo obligatorio 'destinatario', o 'remitente'", 400)
+
+        destinatario = data["destinatario"]
+        remitente = data["remitente"]
+
+        if destinatario == remitente:
+            return responseError("CAMPO_INVALIDO", "El destinatario y remitente no pueden ser iguales", 400)
+
+        return twilio.llamar(destinatario, remitente)
