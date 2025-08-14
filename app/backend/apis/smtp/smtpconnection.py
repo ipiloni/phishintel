080853a8a1d183619fdf667249ac53eeb5980b9a
MarcosGurruchaga <mgurruchaga@frba.utn.edu.ar>
2025-08-14 14:23:42 -0300

    Arreglo tema logs en smtpconnection.py y quito el resultado evento en enviar mail

--- app/backend/apis/smtp/smtpconnection.py
+++ app/backend/apis/smtp/smtpconnection.py
@@ -3,7 +3,7 @@ import traceback
 from socket import gaierror
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from app.utils import logger
+from app.utils.logger import log
 
 class SMTPConnection:
     def __init__(self, host, port):
@@ -22,58 +22,58 @@ class SMTPConnection:
         try:
             self.server.ehlo()
             if not self.server.does_esmtp:
-                logger.error('The server does not support ESMTP')
+                log.error('The server does not support ESMTP')
                 exit(1)
         except smtplib.SMTPHeloError:
-            logger.error('The server did not reply properly to the EHLO/HELO greeting.')
+            log.error('The server did not reply properly to the EHLO/HELO greeting.')
             exit(1)
 
     def __connect(self):
         try:
-            logger.info('Connecting to SMTP socket (' + self.socket + ')...')
+            log.info('Connecting to SMTP socket (' + self.socket + ')...')
             self.server = smtplib.SMTP(self.host, self.port)
         except (gaierror, OSError):
-            logger.error('Unable to establish connection to SMTP socket.')
+            log.error('Unable to establish connection to SMTP socket.')
             exit(1)
 
     def __start_tls(self):
         self.__ehlo()
         if not self.server.has_extn('starttls'):
-            logger.error('SMTP server does not support TLS.')
+            log.error('SMTP server does not support TLS.')
             exit(1)
         else:
             try:
-                logger.info('Starting TLS session...')
+                log.info('Comenzando Sesion TLS')
                 self.server.starttls()
             except RuntimeError:
-                logger.error('SSL/TLS support is not available to your Python interpreter.')
+                log.error('SSL/TLS support is not available to your Python interpreter.')
                 exit(1)
 
     def __eval_server_features(self):
         self.__ehlo()
 
         if not self.server.has_extn('auth'):
-            logger.error('No AUTH types detected.')
+            log.error('No AUTH types detected.')
             exit(1)
 
         server_auth_features = self.server.esmtp_features.get('auth').strip().split()
         supported_auth_features = { auth_type for auth_type in {'PLAIN', 'LOGIN'} if auth_type in server_auth_features }
 
         if not supported_auth_features:
-            logger.error('SMTP server does not support AUTH PLAIN or AUTH LOGIN.')
+            log.error('SMTP server does not support AUTH PLAIN or AUTH LOGIN.')
             exit(1)
 
     def login(self, username, password):
         try:
             return self.server.login(username, password)
         except smtplib.SMTPAuthenticationError:
-            logger.error('The server did not accept the username/password combination.')
+            log.error('The server did not accept the username/password combination.')
             return False
         except smtplib.SMTPNotSupportedError:
-            logger.error('The AUTH command is not supported by the server.')
+            log.error('The AUTH command is not supported by the server.')
             exit(1)
         except smtplib.SMTPException:
-            logger.error('Encountered an error during authentication.')
+            log.error('Encountered an error during authentication.')
             exit(1)
 
     def compose_message(self, sender, name, recipients, subject, html):
@@ -93,10 +93,10 @@ class SMTPConnection:
 
     def send_mail(self, message):
         try:
-            logger.info('Sending spoofed message...')
+            log.info('Enviando Spoofing')
             self.server.sendmail(self.sender, self.recipients, message.as_string())
-            logger.success('Message sent!')
+            log.info('Mensaje enviado correctamente!')
         except smtplib.SMTPException:
-            logger.error('Unable to send message. Check sender, recipients and message body')
-            logger.error(traceback.format_exc())
+            log.error('Unable to send message. Check sender, recipients and message body')
+            log.error(traceback.format_exc())
             exit(1)
