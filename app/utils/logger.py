7132e56d65d951c601f06941129a2c79d6b473a6
MarcosGurruchaga <mgurruchaga@frba.utn.edu.ar>
2025-08-14 14:38:33 -0300

    Log.Success en verde agregado

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,5 +1,17 @@
 import logging
 
+# Success customizado
+SUCCESS_LEVEL_NUM = 25  # Entre INFO y WARNING
+logging.addLevelName(SUCCESS_LEVEL_NUM, "SUCCESS")
+
+def success(self, message, *args, **kwargs):
+    if self.isEnabledFor(SUCCESS_LEVEL_NUM):
+        # ANSI green color for the text
+        message = f"\033[92m{message}\033[0m"
+        self._log(SUCCESS_LEVEL_NUM, message, args, **kwargs)
+
+logging.Logger.success = success
+
 # Configuración básica del logger
 logging.basicConfig(
     level=logging.INFO,  # Nivel mínimo a registrar (INFO, WARNING, ERROR, etc.)
