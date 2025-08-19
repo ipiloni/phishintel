1f7099605bde34bd7adf130a20df95120350bff8
ipiloni <ignaciopiloni@hotmail.com>
2025-08-19 20:33:53 -0300

    Exponemos audios a internet, seguimos con el flujo de respuesta y procesamiento
    Co-Authored-By: MoraRodriguez <81938290+MoraRodriguez@users.noreply.github.com>

--- app/controllers/llamadas/llamadasController.py
+++ app/controllers/llamadas/llamadasController.py
@@ -1,5 +1,12 @@
+from flask import Response, jsonify
+from twilio.twiml.voice_response import VoiceResponse
+
+from app.apis import enviarAudio
+from app.backend.apis.ai import gemini
 from app.backend.apis.twilio import twilio
 from app.backend.models.error import responseError
+from app.controllers.llamadas.elevenLabsController import ElevenLabsController
+from app.utils.logger import log
 
 class LlamadasController:
 
@@ -17,14 +24,6 @@ class LlamadasController:
             return responseError("CAMPO_INVALIDO", "El destinatario y remitente no pueden ser iguales", 400)
 
         return twilio.llamar(destinatario, remitente)
-
-    # @staticmethod
-    # def procesarRespuesta(algoDeTwilio):
-    #     # informacion = lo que responde la persona = lo que te da twilio de llamada ahora.
-    #     # 1. procesar esta info
-    #     # 2. anidar esta informacion (en formato texto) a la conversacion (sumarlo a la lista conversacion)
-    #     # 3. generar una respuesta
-    #     # 4. enviar a twilio un audio con la respuesta
 
     # @staticmethod
     # def generarLlamada(idDestinatario, idRemitente, objetivo):
@@ -49,9 +48,44 @@ class LlamadasController:
     #     # Devuelve la URL absoluta del endpoint (para pasársela al create call)
     #     return url_for("audio_endpoint", _external=True)
 
-
-
-
     # @staticmethod
     # def generarRespuesta(data):
     #     if not data or "conversacion" not in data or "objetivo" not in data:
+
+    @staticmethod
+    def procesarRespuesta(speech, confidence):
+        # if confidence es bajo => quiere decir que la transcripcion no fue buena (no se entendio), podemos pedirle al usuario que repita lo que dijo.
+        # speech ya es el texto de lo que dijo, esta transcripto
+
+        log.info(f"Lo que dijo el usuario fue: {str(speech)}")
+
+        # la conversacion deberia estar en memoria para no tardar tiempo. Como va a ser siempre 1 a la vez y no mas, podemos hacerlo
+        conversacion = []
+
+        conversacion.append(speech)
+        # 3. generar una respuesta
+        respuestaEnTexto = gemini.generarRespuesta(conversacion)
+
+        # 3.1. armar el audio
+        data = jsonify({
+            "texto": respuestaEnTexto,
+            "idVoz": "O1CnH2NGEehfL1nmYACp", # TODO: obtenerlo de la conversacion, del remitente!
+            "estabilidad": 0.5,
+            "velocidad": "normal"
+        })
+        audio = ElevenLabsController.generarTTS(data)
+        
+        # 3.2. exponer audio en internet
+        urlAudio = enviarAudio(audio)
+
+        # 4. enviar a twilio un audio con la respuesta
+        response = VoiceResponse()
+        response.play(urlAudio)
+
+        response.gather(
+            input="speech",
+            timeout=5,
+            action="https://a552b2577df7.ngrok-free.app/api/twilio/respuesta" # TODO: este endpoint es nuestro. Cuando pase al despliegue se debe cambiar 
+        )
+        
+        return Response(str(response), mimetype="text/xml")
