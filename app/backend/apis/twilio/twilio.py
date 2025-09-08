1a9c2b6bd1e9ee397b07602d33eaac73eb0ec453
ipiloni <ignaciopiloni@hotmail.com>
2025-09-08 12:13:33 -0300

    agrego un poco de logica en las llamadas

--- app/backend/apis/twilio/twilio.py
+++ app/backend/apis/twilio/twilio.py
@@ -2,26 +2,25 @@ from flask import jsonify
 from twilio.rest import Client
 
 from app.utils.config import get
+from app.utils.logger import log
 
 account_sid = get("TWILIO_ACCOUNT_SID_MORA")
 auth_token = get("TWILIO_AUTH_TOKEN_MORA")
 
 client = Client(account_sid, auth_token)
 
-def llamar(destinatario, remitente):
-  call = client.calls.create(
-    url="http://demo.twilio.com/docs/voice.xml",
-    to=destinatario,
-    from_=remitente,
-  )
-  print(f"Imprimo resultados")
+def llamar(destinatario, remitente, url_audio):
+    log.info(f"Se recibio una solicitud para llamar al destinatario {str(destinatario)}")
 
-  print(call.sid)
-  print(call.duration)
-  print(call.status)
-  print(call.direction)
+    call = client.calls.create(
+      url=url_audio,
+      to=destinatario,
+      from_=remitente,
+    )
 
-  return jsonify({
-    'sid': call.sid,
-    'status': call.status
-  })
+    log.info(f"Resultados actuales: {call.sid} - {call.duration} - {call.status} - {call.direction}")
+
+    return jsonify({
+      'sid': call.sid,
+      'status': call.status
+    })
