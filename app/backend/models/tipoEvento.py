ffa868abffa8468675acdd68859c5cbabde790ae
juliangarcia45 <judanielplate@hotmail.com>
2025-10-09 21:38:41 -0300

    arreglo filtro periodo en reporte

--- app/backend/models/tipoEvento.py
+++ app/backend/models/tipoEvento.py
@@ -5,5 +5,6 @@ class TipoEvento(enum.Enum):
     LLAMADA_WPP = "LLAMADA_WPP" # Llamada + WPP
     LLAMADA_CORREO = "LLAMADA_CORREO" # Llamada + Correo
     LLAMADA = "LLAMADA" # Llamada simple (Para pedir información o algo por el estilo, por favor no borrar)
+    VIDEOLLAMADA = "VIDEOLLAMADA" 
     CORREO = "CORREO" # Correo simple (Un correo llega a tu casilla)
     MENSAJE = "MENSAJE" # Mensaje simple (Un mensaje llega a tu casilla, puede ser tanto SMS como WPP)
